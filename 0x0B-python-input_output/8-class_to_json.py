b3204a20abfc3a5b707fd88206f0d6617dcb0ed8
Maryjane-16 <okwuazichinazor@gmail.com>
2023-04-12 15:40:57 -0700

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,20 @@
-"""Defines a Python class-to-JSON function."""
+#!/usr/bin/python3
+"""``class_to_json`` module"""
 
 
 def class_to_json(obj):
-    """Return the dictionary represntation of a simple data structure."""
-    return obj.__dict__
+    """Returns the dictionary description with simple data structures:
+        - list
+        - dictionary
+        - string
+        - integer
+        - boolean
+        for JSON
+    """
+    r = {}
+    for key in obj.__dict__:
+        value = getattr(obj, key)
+        if type(value) in [list, dict, str, int, bool]:
+            r[key] = value
+
+    return r
